5b1cb6668aa85263d9716e21a052119fa8c17d69
davi894 <moisesdavi005@gmail.com>
2023-06-28 13:58:59 -0300

    Add styled in home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
+
 import Header from "@/components/Header/Header";
 import Button from "./components/Button/Button";
 
-export default function Home() {
+export default async function Home() {
+
     return (
-        <main className="h-full">
+        <main className="h-full flex">
             <Header />
 
             <article className=" m-0 flex justify-start bg-slate-100 w-32 h-full " >
@@ -43,6 +45,21 @@ export default function Home() {
                 </section>
 
             </article>
+
+            <ul className="mt-20 ml-20 flex flex-wrap w-full">
+
+                <li className="flex flex-col bg-slate-950 h-48 w-48 p-2 rounded-t-md">
+                    <h2 className="font-black text-white text-xl">Address</h2>
+                    <span className="text-white">street: </span>
+                    <span className="text-white"> number: </span>
+                    <span className="text-white">city:  </span>
+                    <span className="text-white">state: </span>
+                    <span className="text-white">postalCode:  </span>
+                    <span className="text-white">country:  </span>
+                    <img src="" alt="" />
+                </li>
+
+            </ul>
 
         </main>
     );
