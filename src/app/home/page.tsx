bc1d1ebd44da556e12bb0a79cdffa5bf033a4140
davi894 <moisesdavi005@gmail.com>
2023-07-13 15:46:06 -0300

    chore: nutton component in home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
         <main className="h-full flex">
             <Header />
 
-            <article className=" m-0 flex justify-start bg-slate-100 w-32 h-full " >
+            <article className="m-0 flex justify-start bg-slate-100 w-32 h-full " >
 
                 <section className="ml-1.5 items-start flex flex-col mt-20 space-y-3" >
 
